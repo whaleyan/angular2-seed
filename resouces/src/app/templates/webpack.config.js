07fe8b7052f3e844a55cb56eb73147f0ffa50d03
whale.paul <yanmjman@163.com>
2017-04-05 10:56:50 +0800

    add

---
+++ resouces/src/app/templates/webpack.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/12/6 0006.
+ */
+module.exports = {
+    // 入口
+    entry: "./hello.js",
+    // 输出的文件名
+    output: {
+        filename: 'bundle.js'
+    }
+};
